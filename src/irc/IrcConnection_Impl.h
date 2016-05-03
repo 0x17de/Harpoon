89a53a2844b56f4470015421410037b3d8703630
Manuel Herrmann <git@0x17.de>
2016-05-03 23:46:35 +0200

    some comments

--- src/irc/IrcConnection_Impl.h
+++ src/irc/IrcConnection_Impl.h
@@ -8,29 +8,42 @@ class IrcConnection_Impl {
 public:
 	irc_session_t* ircSession;
 
+	// event callback typedef
 	typedef void (ircEventCallback_t)(irc_session_t* session,
 		const char* event,
 		const char* origin,
 		const char** params,
 		unsigned int count);
 
+	// various event callbacks for libircclient
+
+	// connectivity
 	ircEventCallback_t onConnect;
 	ircEventCallback_t onNick;
 	ircEventCallback_t onQuit;
 	ircEventCallback_t onJoin;
 	ircEventCallback_t onPart;
+
+	// modes
 	ircEventCallback_t onMode;
 	ircEventCallback_t onUmode;
+
+	// channel
 	ircEventCallback_t onTopic;
 	ircEventCallback_t onKick;
 	ircEventCallback_t onChannel;
 	ircEventCallback_t onPrivmsg;
+
+	// server
 	ircEventCallback_t onNotice;
 	ircEventCallback_t onInvite;
+
+	// client2client
 	ircEventCallback_t onReq;
 	ircEventCallback_t onRep;
-	ircEventCallback_t onAction;
-	ircEventCallback_t onUnknown;
+
+	ircEventCallback_t onAction; // me
+	ircEventCallback_t onUnknown; // others
 };
 #warning STUB IrcConnection_Impl
 
--- src/queue/EventQueue.cpp
+++ src/queue/EventQueue.cpp
@@ -26,21 +26,24 @@ void EventQueue::sendEvent(std::shared_ptr<IEvent> event) {
 int EventQueue::getEvent(int timeout /* milliseconds */, std::shared_ptr<IEvent>& event) {
 	std::chrono::milliseconds timeoutMillis(timeout);
 	std::unique_lock<std::timed_mutex> lock(impl->queueMutex, std::defer_lock);
-	bool lockResult = lock.try_lock_for(timeoutMillis);
+	bool lockResult = lock.try_lock_for(timeoutMillis); // lock for checking queue fill status
 	if (!lockResult)
 		return false;
 
 	if (impl->events.size() == 0) {
 		lock.unlock();
 		std::unique_lock<std::mutex> emptyLock(impl->emptyQueueMutex);
+		// wait until queue is filled or timeout
 		std::cv_status waitResult = impl->eventCondition.wait_for(emptyLock, timeoutMillis);
 		if (waitResult == std::cv_status::timeout)
 			return false;
+		// try to lock queue
 		lockResult = lock.try_lock_for(timeoutMillis);
 		if (!lockResult)
 			return false;
 	}
 
+	// get and remove event from queue
 	event = impl->events.front();
 	impl->events.pop_front();
 	return true;
