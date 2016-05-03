df2831ef88cb3dd145f6c989bea4e59ed33b5081
Manuel Herrmann <0@0x17.de>
2016-05-03 22:45:18 +0200

    irc connection function stubs
    Signed-off-by: Manuel Herrmann <0@0x17.de>

--- src/irc/IrcConnection_Impl.h
+++ src/irc/IrcConnection_Impl.h
@@ -4,16 +4,21 @@
 #include <libircclient.h>
 #include <list>
 
-
 class IrcConnection_Impl {
 public:
 	irc_session_t* ircSession;
 
-	void onConnect(irc_session_t* session,
+	typedef void (ircEventCallback_t)(irc_session_t* session,
 		const char* event,
 		const char* origin,
 		const char** params,
 		unsigned int count);
+
+	ircEventCallback_t onConnect;
+	ircEventCallback_t onNick;
+	ircEventCallback_t onQuit;
+	ircEventCallback_t onJoin;
+	ircEventCallback_t onPart;
 };
 #warning STUB IrcConnection_Impl
 
