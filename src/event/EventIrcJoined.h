2ed3bf14e3f9108d66d9246c4881b7b33c353400
Manuel Herrmann <0@0x17.de>
2016-05-06 23:16:57 +0200

    EventIrcJoined
    Signed-off-by: Manuel Herrmann <0@0x17.de>

---
+++ src/event/EventIrcJoined.h
@@ -0,0 +1,22 @@
+#ifndef EVENTIRCJOINED_H
+#define EVENTIRCJOINED_H
+
+#include "IEvent.h"
+#include <string>
+
+
+class EventIrcJoined : public IEvent {
+	size_t serverId;
+	std::string username;
+	std::string channel;
+public:
+	static UUID uuid;
+	virtual UUID getEventUuid() const override;
+
+	EventIrcJoined(size_t serverId, std::string username, std::string channel);
+	size_t getServerId();
+	std::string getUsername();
+	std::string getChannel();
+};
+
+#endif
