c7d10138dbc6f2e4ed566c88d1fccbc9c82023fa
Manuel Herrmann <git@0x17.de>
2016-05-07 03:51:53 +0200

    crypto+base64 header

--- src/utils/Crypto.cpp
+++ src/utils/Crypto.cpp
@@ -1,8 +1,15 @@
 #include "Crypto.hpp"
+#include "Base64.hpp"
+#include <stdexcept>
+#include <string>
+#include <vector>
 #include <openssl/conf.h>
 #include <openssl/err.h>
+#include <openssl/rand.h>
 #include <openssl/evp.h>
 #include <openssl/crypto.h>
+
+using namespace std;
 
 
 static int cryptoCount = 0;
@@ -26,3 +33,30 @@ Crypto::~Crypto() {
 		ERR_free_strings();
 	}
 }
+
+std::string Crypto::genSalt(size_t size) {
+	unsigned char bytes[size];
+	if (!RAND_bytes(bytes, size))
+		throw runtime_error("Can not generate salt.");
+	return string(reinterpret_cast<char*>(bytes), size);
+}
+
+std::string Crypto::generateKey(const std::string& salt, const std::string& password, size_t keyLength) {
+	vector<unsigned char> buffer(keyLength, 0);
+	if (!PKCS5_PBKDF2_HMAC(password.c_str(), password.size(),
+		reinterpret_cast<const unsigned char*>(salt.c_str()), salt.size(), 40000,
+		EVP_sha512(),
+		buffer.size(), buffer.data()))
+	{
+		throw runtime_error("Can not hash password.");
+	}
+	return string(reinterpret_cast<char*>(buffer.data()), buffer.size());
+}
+
+std::string Crypto::hashPassword(const std::string& salt, const std::string& password, size_t keyLength) {
+	string key = generateKey(salt, password, keyLength);
+	Base64 b;
+	vector<char> encodedLength(b.encodedLength(keyLength), 0);
+	return b.encode(key);
+}
+
