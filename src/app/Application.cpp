88d475fe21422bd199e3b6fdbf71eb630b905faf
Manuel Herrmann <0@0x17.de>
2016-05-09 00:11:07 +0200

    removed TRANSMIT_EVENT macro from application.cpp
    Signed-off-by: Manuel Herrmann <0@0x17.de>

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -19,9 +19,9 @@ Application::Application() :
 {
 	EventQueue* queue = getEventQueue();
 	userManager = make_shared<UserManager>(queue);
+	eventHandlers.push_back(userManager);
 	eventHandlers.push_back(make_shared<ChatDatabaseDummy>(queue));
-	//eventHandlers.push_back(make_shared<>());
-	//server1 = make_shared<>(queue);
+
 #ifdef USE_WEBSOCKET_SERVER
 	eventHandlers.push_back(make_shared<WebsocketServer>(queue));
 #endif
@@ -34,23 +34,17 @@ bool Application::onEvent(std::shared_ptr<IEvent> event) {
 	// which event do we process?
 	UUID eventType = event->getEventUuid();
 
-	#define TRANSMIT_EVENT(theQueue) \
-		EventQueue* theQueue##Queue = theQueue->getEventQueue(); \
-		if (theQueue##Queue->canProcessEvent(eventType)) \
-			theQueue##Queue->sendEvent(event);
-	TRANSMIT_EVENT(userManager)
-	userManager->sendEventToUser(event); // sends only to matching user (only if IUserEvent)
 	for (auto& eventHandler : eventHandlers) {
-		TRANSMIT_EVENT(eventHandler)
+		auto eventQueue = eventHandler->getEventQueue();
+		if (eventQueue->canProcessEvent(eventType))
+			eventQueue->sendEvent(event);
 	}
-	#undef TRANSMIT_EVENT
+	userManager->sendEventToUser(event); // sends only to matching user (only if IUserEvent)
 
 	if (eventType == EventQuit::uuid) {
 		cout << "Received Quit Event" << endl;
-		userManager->join();
 		for (auto& eventHandler : eventHandlers)
 			eventHandler->join();
-		//server1->join();
 		cout << "Submodules were stopped" << endl;
 		return false;
 	}
