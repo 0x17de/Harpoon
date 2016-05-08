2e9da00199f6910408c3d7dba32455a1e98d8c89
Manuel Herrmann <git@0x17.de>
2016-05-08 17:21:01 +0200

    print all events out for the start (except ctcp)

--- src/irc/IrcEvents.cpp
+++ src/irc/IrcEvents.cpp
@@ -25,6 +25,10 @@ void IrcConnection_Impl::onNick(irc_session_t* session,
 	unsigned int count)
 {
 #warning stub onNick
+	if (count < 1) return;
+	string originalNick(origin);
+	string newNick(params[0]);
+	cout << "Nickchange<" << originalNick << ">: " << newNick << endl;
 }
 void IrcConnection_Impl::onQuit(irc_session_t* session,
 	const char* event,
@@ -33,6 +37,13 @@ void IrcConnection_Impl::onQuit(irc_session_t* session,
 	unsigned int count)
 {
 #warning stub onQuit
+	string who(origin);
+	if (count < 1) {
+		cout << "Q<" << origin << ">" << endl;
+	} else {
+		string reason(params[0]);
+		cout << "Q<" << origin << ">: " << reason << endl;
+	}
 }
 void IrcConnection_Impl::onJoin(irc_session_t* session,
 	const char* event,
@@ -40,9 +51,15 @@ void IrcConnection_Impl::onJoin(irc_session_t* session,
 	const char** params,
 	unsigned int count)
 {
-	string username(origin);
+	string who(origin);
 	string channel(params[0]);
 	make_shared<EventIrcJoined>(configuration.serverId, origin, channel);
+	if (count < 2) {
+		cout << "JOIN<" << who << ">: " << channel << endl;
+	} else {
+		string reason(params[1]);
+		cout << "JOIN<" << who << ">: " << channel << ": " << reason << endl;
+	}
 }
 void IrcConnection_Impl::onPart(irc_session_t* session,
 	const char* event,
@@ -51,9 +68,15 @@ void IrcConnection_Impl::onPart(irc_session_t* session,
 	unsigned int count)
 {
 #warning stub onPart
-	//string username(origin);
-	//string channel(params[0]);
+	string who(origin);
+	string channel(params[0]);
 	//make_shared...
+	if (count < 2) {
+		cout << "PART<" << who << ">: " << channel << endl;
+	} else {
+		string reason(params[1]);
+		cout << "PART<" << who << ">: " << channel << ": " << reason << endl;
+	}
 }
 void IrcConnection_Impl::onMode(irc_session_t* session,
         const char* event,
@@ -62,6 +85,15 @@ void IrcConnection_Impl::onMode(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onMode
+	string who(origin);
+	string channel(params[0]);
+	string mode(params[1]);
+	if (count < 3) {
+		cout << "MODE<" << who << ">: " << channel << " " << mode << endl;
+	} else {
+		string arg(params[2]);
+		cout << "MODE<" << who << ">: " << channel << " " << mode << ": " << arg << endl;
+	}
 }
 void IrcConnection_Impl::onUmode(irc_session_t* session,
         const char* event,
@@ -70,6 +102,15 @@ void IrcConnection_Impl::onUmode(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onUmode
+	string who(origin);
+	string channel(params[0]);
+	string mode(params[1]);
+	if (count < 3) {
+		cout << "UMODE<" << who << ">: " << channel << " " << mode << endl;
+	} else {
+		string arg(params[2]);
+		cout << "UMODE<" << who << ">: " << channel << " " << mode << ": " << arg << endl;
+	}
 }
 void IrcConnection_Impl::onTopic(irc_session_t* session,
         const char* event,
@@ -78,6 +119,10 @@ void IrcConnection_Impl::onTopic(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onTopic
+	string who(origin);
+	string channel(params[0]);
+	string topic(params[1]);
+	cout << "TOPIC<" << who << ">: " << channel << ": " << topic << endl;
 }
 void IrcConnection_Impl::onKick(irc_session_t* session,
         const char* event,
@@ -95,10 +140,10 @@ void IrcConnection_Impl::onChannel(irc_session_t* session,
 {
 #warning stub onChannel
 	if (count < 2) return;
-	string username(origin);
-	string self(params[0]);
+	string who(origin);
+	string channel(params[0]);
 	string message(params[1]);
-	cout << "<" << username << ">: " << message << endl;
+	cout << "<" << channel << ":" << who << ">: " << message << endl;
 }
 void IrcConnection_Impl::onPrivmsg(irc_session_t* session,
         const char* event,
@@ -108,10 +153,10 @@ void IrcConnection_Impl::onPrivmsg(irc_session_t* session,
 {
 #warning stub onPrivmsg
 	if (count < 2) return;
-	string username(origin);
+	string who(origin);
 	string self(params[0]);
 	string message(params[1]);
-	cout << "<" << username << ">: " << message << endl;
+	cout << "<" << who << "|" << self << ">: " << message << endl;
 }
 void IrcConnection_Impl::onNotice(irc_session_t* session,
         const char* event,
@@ -120,6 +165,11 @@ void IrcConnection_Impl::onNotice(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onNotice
+	if (count < 2) return;
+	string who(origin);
+	string self(params[0]);
+	string message(params[1]);
+	cout << "N<" << who << "|" << self << ">: " << message << endl;
 }
 void IrcConnection_Impl::onChannelNotice(irc_session_t* session,
         const char* event,
@@ -128,6 +178,11 @@ void IrcConnection_Impl::onChannelNotice(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onChannelNotice
+	if (count < 2) return;
+	string who(origin);
+	string self(params[0]);
+	string message(params[1]);
+	cout << "CN<" << who << "|" << self << ">: " << message << endl;
 }
 void IrcConnection_Impl::onInvite(irc_session_t* session,
         const char* event,
@@ -136,6 +191,11 @@ void IrcConnection_Impl::onInvite(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onInvite
+	if (count < 2) return;
+	string who(origin);
+	string nick(params[0]);
+	string channel(params[1]);
+	cout << "Invite<" << who << ">: " << channel << ": " << nick << endl;
 }
 void IrcConnection_Impl::onCtcpReq(irc_session_t* session,
         const char* event,
@@ -168,5 +228,7 @@ void IrcConnection_Impl::onUnknown(irc_session_t* session,
         unsigned int count)
 {
 #warning stub onUnknown
+	string who(origin);
+	cout << "UnknownEvent<" << who << ">" << endl;
 }
 
