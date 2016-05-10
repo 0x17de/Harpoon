e494d6dcfbde355c7666d4f432fd6b97967b73b9
Manuel Herrmann <git@0x17.de>
2016-05-10 14:09:24 +0200

    login rethought

--- src/event/EventLogin.hpp
+++ src/event/EventLogin.hpp
@@ -5,11 +5,13 @@
 #include "IEvent.hpp"
 
 
+class EventQueue;
 class EventLogin : public IEvent {
 public:
 	static UUID uuid;
 	virtual UUID getEventUuid() const override;
 	//std::shared_ptr<IClient> client;
+	EventQueue* target;
 	std::string username;
 	std::string password;
 };
