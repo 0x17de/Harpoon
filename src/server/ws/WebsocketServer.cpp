6ea1fe4a1c06692c8f3e83ab4dfabcfb3cbe300c
Manuel Herrmann <0@0x17.de>
2016-05-10 13:22:42 +0200

    websocket server basic structure
    Signed-off-by: Manuel Herrmann <0@0x17.de>

--- src/server/ws/WebsocketServer.cpp
+++ src/server/ws/WebsocketServer.cpp
@@ -1,11 +1,15 @@
 #include "WebsocketServer.hpp"
 #include "WebsocketServer_Impl.hpp"
+#include "event/IUserEvent.hpp"
 #include "event/EventQuit.hpp"
+#include <iostream>
 
 using namespace std;
 
 
 WebsocketServer::WebsocketServer(EventQueue* appQueue)
+:
+	impl{new WebsocketServer_Impl(appQueue)}
 {
 }
 
@@ -13,6 +17,17 @@ WebsocketServer::~WebsocketServer() {
 }
 
 bool WebsocketServer::onEvent(std::shared_ptr<IEvent> event) {
+	return impl->onEvent(event);
+}
+
+WebsocketServer_Impl::WebsocketServer_Impl(EventQueue* appQueue)
+:
+	appQueue{appQueue}
+{
+
+}
+
+bool WebsocketServer_Impl::onEvent(std::shared_ptr<IEvent> event) {
 	UUID eventType = event->getEventUuid();
 	if (eventType == EventQuit::uuid) {
 		return false;
@@ -21,6 +36,15 @@ bool WebsocketServer::onEvent(std::shared_ptr<IEvent> event) {
 }
 
 void WebsocketServer::sendEventToUser(std::shared_ptr<IEvent> event) {
-
+	auto userEvent = event->as<IUserEvent>();
+	if (userEvent != nullptr) {
+		cout << "NonNULL event" << endl;
+		//auto it = users.find(userEvent->getUserId());
+		//if (it != users.end()) {
+			//EventQueue* queue = it->second.getEventQueue();
+			//if (queue->canProcessEvent(event->getEventUuid()))
+			//	queue->sendEvent(event);
+		//}
+	}
 }
 
