69c715499ac025fb13b1563da149b5c1b135351c
Manuel Herrmann <0@0x17.de>
2016-05-13 17:31:13 +0200

    irc connected event
    Signed-off-by: Manuel Herrmann <0@0x17.de>

---
+++ src/event/EventIrcConnected.hpp
@@ -0,0 +1,20 @@
+#ifndef EVENTIRCCONNECTED_H
+#define EVENTIRCCONNECTED_H
+
+#include "IEvent.hpp"
+#include <cstdlib>
+
+
+class EventIrcConnected : public IEvent {
+	size_t userId;
+	size_t serverId;
+public:
+	static UUID uuid;
+	virtual UUID getEventUuid() const override;
+
+	EventIrcConnected(size_t userId, size_t serverId);
+	size_t getUserId() const;
+	size_t getServerId() const;
+};
+
+#endif
