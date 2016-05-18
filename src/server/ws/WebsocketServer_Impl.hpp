4c05850e6a9bbea5504a09bf8971ce874c84f5e6
Manuel Herrmann <git@0x17.de>
2016-05-19 01:26:40 +0200

    websocket server basic handler stubs

--- src/server/ws/WebsocketServer_Impl.hpp
+++ src/server/ws/WebsocketServer_Impl.hpp
@@ -1,13 +1,21 @@
 #ifndef WEBSOCKETSERVER_IMPL_H
 #define WEBSOCKETSERVER_IMPL_H
 #include <memory>
+#include <unordered_map>
 #include <WebSocket.h>
+
+using namespace seasocks;
 
 
 class EventQueue;
-class WebsocketServer_Impl {
+class WebsocketServer_Impl : public WebSocket::Handler {
+	std::unordered_map<WebSocket*, size_t> _connections;
 public:
 	EventQueue* appQueue;
+
+	virtual void onConnect(WebSocket* connection);
+	virtual void onData(WebSocket* connection, const char* data);
+	virtual void onDisconnect(WebSocket* connection);
 
 	WebsocketServer_Impl(EventQueue* appQueue);
 	bool onEvent(std::shared_ptr<IEvent> event);
