224a04752723d8f5258101d39bcfb485df4f024b
Manuel Herrmann <git@0x17.de>
2016-05-31 01:45:26 +0200

    irc chat listing structs

--- src/event/irc/IrcChatListing.hpp
+++ src/event/irc/IrcChatListing.hpp
@@ -4,6 +4,20 @@
 #include "../EventChatListing.hpp"
 
 
+struct IrcChannelListing {
+	IrcChannelListing(const std::string& name);
+
+	std::string name;
+};
+
+struct IrcServerListing {
+	IrcServerListing(size_t id, const std::string& name);
+
+	size_t id;
+	std::string name;
+	std::list<IrcChannelListing> channels;
+};
+
 class IrcChatListing : public ChatListing {
 public:
 	static UUID uuid;
