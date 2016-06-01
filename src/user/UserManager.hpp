44ac21529ec4db35cfd7307a4044f07a20f024fa
Manuel Herrmann <0@0x17.de>
2016-06-01 03:09:10 +0200

    allow multiple services
    Signed-off-by: Manuel Herrmann <0@0x17.de>

--- src/user/UserManager.hpp
+++ src/user/UserManager.hpp
@@ -10,7 +10,7 @@ class EventQueue;
 class EventLoginResult;
 class UserManager : public ManagingEventLoop {
 	EventQueue* appQueue;
-	std::map<size_t, std::shared_ptr<EventLoop>> users;
+	std::map<size_t, std::map<size_t, std::shared_ptr<EventLoop>>> users;
 public:
 	UserManager(EventQueue* appQueue);
 	virtual bool onEvent(std::shared_ptr<IEvent> event) override;
