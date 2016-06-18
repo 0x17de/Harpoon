8f59f28383716c960c048529661312f154558fe0
Manuel Herrmann <git@0x17.de>
2016-06-18 23:58:45 +0200

    password utils files

---
+++ src/utils/Password.hpp
@@ -0,0 +1,18 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+#include <string>
+
+
+class Password {
+	std::string salt;
+	std::string hash;
+public:
+	Password(const std::string& password);
+	Password(const std::string& salt, const std::string& password);
+	std::string getSaltBase64() const;
+	std::string getHashBase64() const;
+	bool equals(const std::string& hash);
+};
+
+#endif
