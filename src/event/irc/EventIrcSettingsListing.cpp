01a14fa570559c98e53362956f1b83d144da9859
Manuel Herrmann <git@0x17.de>
2016-06-29 00:20:21 +0200

    event irc settings listing stub

---
+++ src/event/irc/EventIrcSettingsListing.cpp
@@ -0,0 +1,35 @@
+#include "EventIrcSettingsListing.hpp"
+#include "utils/uuidGen.hpp"
+#include "service/irc/IrcChannelLoginData.hpp"
+#include "service/irc/IrcServerHostConfiguration.hpp"
+#include "service/irc/IrcServerConfiguration.hpp"
+
+
+UUID EventIrcSettingsListing::uuid = ::uuid.get();
+UUID EventIrcSettingsListing::getEventUuid() const {
+    return this->uuid;
+}
+
+EventIrcSettingsListing::EventIrcSettingsListing(size_t userId, void* data)
+    : userId{userId}
+    , data{data}
+{
+}
+
+IrcServerConfiguration& EventIrcSettingsListing::addServer(size_t serverId, std::string serverName) {
+    servers.emplace_back(serverId, serverName);
+    return servers.back();
+}
+
+size_t EventIrcSettingsListing::getUserId() const {
+    return userId;
+}
+
+const std::list<IrcServerConfiguration>& EventIrcSettingsListing::getServerList() const {
+    return servers;
+}
+
+void* EventIrcSettingsListing::getData() const {
+    return data;
+}
+
