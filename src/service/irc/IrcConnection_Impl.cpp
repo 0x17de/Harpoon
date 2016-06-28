592a8d85a2531592003983b9bb1a731852e54fdd
Manuel Herrmann <git@0x17.de>
2016-06-28 03:54:44 +0200

    auto indentation fix

--- src/service/irc/IrcConnection_Impl.cpp
+++ src/service/irc/IrcConnection_Impl.cpp
@@ -65,8 +65,8 @@ void onIrcNumeric(irc_session_t* session,
 bool IrcConnection_Impl::findUnusedNick(std::string& nick) {
     auto& nicks = this->configuration.nicks;
     auto nickIt = find_if(nicks.begin(), nicks.end(), [this](std::string& foundNick) {
-            return inUseNicks.find(foundNick) == inUseNicks.end(); // not used
-        });
+        return inUseNicks.find(foundNick) == inUseNicks.end(); // not used
+    });
     bool success = nickIt != nicks.end();
     if (success) nick = *nickIt; // assign nick
     return success;
@@ -83,70 +83,70 @@ IrcConnection_Impl::IrcConnection_Impl(EventQueue* appQueue,
     , running{true}
 {
     ircLoop = thread([this]{ 
-            irc_callbacks_t callbacks = {0};
-            callbacks.event_connect = &onIrcEvent<&IrcConnection_Impl::onConnect>;
-            callbacks.event_nick = &onIrcEvent<&IrcConnection_Impl::onNick>;
-            callbacks.event_quit = &onIrcEvent<&IrcConnection_Impl::onQuit>;
-            callbacks.event_join = &onIrcEvent<&IrcConnection_Impl::onJoin>;
-            callbacks.event_part = &onIrcEvent<&IrcConnection_Impl::onPart>;
-            callbacks.event_mode = &onIrcEvent<&IrcConnection_Impl::onMode>;
-            callbacks.event_umode = &onIrcEvent<&IrcConnection_Impl::onUmode>;
-            callbacks.event_topic = &onIrcEvent<&IrcConnection_Impl::onTopic>;
-            callbacks.event_kick = &onIrcEvent<&IrcConnection_Impl::onKick>;
-            callbacks.event_channel = &onIrcEvent<&IrcConnection_Impl::onChannel>;
-            callbacks.event_privmsg = &onIrcEvent<&IrcConnection_Impl::onPrivmsg>;
-            callbacks.event_notice = &onIrcEvent<&IrcConnection_Impl::onNotice>;
-            callbacks.event_channel_notice = &onIrcEvent<&IrcConnection_Impl::onNotice>;
-            callbacks.event_invite = &onIrcEvent<&IrcConnection_Impl::onInvite>;
-            //callbacks.event_ctcp_req = &onIrcEvent<&IrcConnection_Impl::onCtcpReq>;
-            //callbacks.event_ctcp_rep = &onIrcEvent<&IrcConnection_Impl::onCtcpRep>;
-            callbacks.event_ctcp_action = &onIrcEvent<&IrcConnection_Impl::onCtcpAction>;
-            callbacks.event_unknown = &onIrcEvent<&IrcConnection_Impl::onUnknown>;
+        irc_callbacks_t callbacks = {0};
+		callbacks.event_connect = &onIrcEvent<&IrcConnection_Impl::onConnect>;
+		callbacks.event_nick = &onIrcEvent<&IrcConnection_Impl::onNick>;
+		callbacks.event_quit = &onIrcEvent<&IrcConnection_Impl::onQuit>;
+		callbacks.event_join = &onIrcEvent<&IrcConnection_Impl::onJoin>;
+		callbacks.event_part = &onIrcEvent<&IrcConnection_Impl::onPart>;
+		callbacks.event_mode = &onIrcEvent<&IrcConnection_Impl::onMode>;
+		callbacks.event_umode = &onIrcEvent<&IrcConnection_Impl::onUmode>;
+		callbacks.event_topic = &onIrcEvent<&IrcConnection_Impl::onTopic>;
+		callbacks.event_kick = &onIrcEvent<&IrcConnection_Impl::onKick>;
+		callbacks.event_channel = &onIrcEvent<&IrcConnection_Impl::onChannel>;
+		callbacks.event_privmsg = &onIrcEvent<&IrcConnection_Impl::onPrivmsg>;
+		callbacks.event_notice = &onIrcEvent<&IrcConnection_Impl::onNotice>;
+		callbacks.event_channel_notice = &onIrcEvent<&IrcConnection_Impl::onNotice>;
+		callbacks.event_invite = &onIrcEvent<&IrcConnection_Impl::onInvite>;
+		//callbacks.event_ctcp_req = &onIrcEvent<&IrcConnection_Impl::onCtcpReq>;
+		//callbacks.event_ctcp_rep = &onIrcEvent<&IrcConnection_Impl::onCtcpRep>;
+		callbacks.event_ctcp_action = &onIrcEvent<&IrcConnection_Impl::onCtcpAction>;
+		callbacks.event_unknown = &onIrcEvent<&IrcConnection_Impl::onUnknown>;
 
-            callbacks.event_numeric = &onIrcNumeric<&IrcConnection_Impl::onNumeric>;
+		callbacks.event_numeric = &onIrcNumeric<&IrcConnection_Impl::onNumeric>;
 
-            // callbacks.irc_event_dcc_chat_t event_dcc_chat_req = &onIrcEvent<&IrcConnection_Impl::onReq>
-            // callbacks.irc_event_dcc_send_t event_dcc_send_req = &onIrcEvent<&IrcConnection_Impl::onReq>
+		// callbacks.irc_event_dcc_chat_t event_dcc_chat_req = &onIrcEvent<&IrcConnection_Impl::onReq>
+		// callbacks.irc_event_dcc_send_t event_dcc_send_req = &onIrcEvent<&IrcConnection_Impl::onReq>
 
-            ircSession = irc_create_session(&callbacks);
-            // remember session
-            activeIrcConnections.emplace(ircSession, this);
+		ircSession = irc_create_session(&callbacks);
+		// remember session
+		activeIrcConnections.emplace(ircSession, this);
 
-            while (ircSession != 0 && running) {
-                {
-                    lock_guard<mutex> lock(channelLoginDataMutex);
-                    // copy channels to login
-                    for (auto& channel : this->configuration.channels) {
-                        string channelLower = channel.getChannelName();
-                        transform(channelLower.begin(), channelLower.end(), channelLower.begin(), ::tolower);
-                        channelStores.emplace(channelLower, channel.getChannelPassword());
-                    }
+		while (ircSession != 0 && running) {
+			{
+				lock_guard<mutex> lock(channelLoginDataMutex);
+				// copy channels to login
+				for (auto& channel : this->configuration.channels) {
+					string channelLower = channel.getChannelName();
+					transform(channelLower.begin(), channelLower.end(), channelLower.begin(), ::tolower);
+					channelStores.emplace(channelLower, channel.getChannelPassword());
+				}
 
-                    if (!findUnusedNick(nick)) break; // give up
-                }
+				if (!findUnusedNick(nick)) break; // give up
+			}
 
-                // connect to server
-                irc_connect(ircSession,
-                            this->configuration.hostname.c_str(),
-                            this->configuration.port,
-                            this->configuration.password.empty() ? 0 : this->configuration.password.c_str(),
-                            nick.c_str(),
-                            0 /* username */,
-                            0 /* realname */);
-                cout << "[IC] Connect" << endl;
+			// connect to server
+			irc_connect(ircSession,
+						this->configuration.hostname.c_str(),
+						this->configuration.port,
+						this->configuration.password.empty() ? 0 : this->configuration.password.c_str(),
+						nick.c_str(),
+						0 /* username */,
+						0 /* realname */);
+			cout << "[IC] Connect" << endl;
 
-                // run irc event loop
-                irc_run(ircSession);
-                cout << "[IC] Loop finished" << endl;
+			// run irc event loop
+			irc_run(ircSession);
+			cout << "[IC] Loop finished" << endl;
 
-                irc_disconnect(ircSession);
+			irc_disconnect(ircSession);
 
-                if (running) {
-                    // reconnect
-                    this_thread::sleep_for(chrono::seconds(1));
-                }
-            }
-        });
+			if (running) {
+				// reconnect
+				this_thread::sleep_for(chrono::seconds(1));
+			}
+		}
+    });
 }
 
 IrcConnection_Impl::~IrcConnection_Impl() {
