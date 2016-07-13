5892c1b22823751656cff853880680904d0f76f9
Manuel Herrmann <git@0x17.de>
2016-07-13 17:59:01 +0200

    untabify

--- src/service/irc/IrcServerHostConfiguration.hpp
+++ src/service/irc/IrcServerHostConfiguration.hpp
@@ -5,23 +5,23 @@
 
 
 class IrcServerHostConfiguration {
-	std::string hostName;
-	int port;
-	std::string password;
-	bool ipV6;
-	bool ssl;
+    std::string hostName;
+    int port;
+    std::string password;
+    bool ipV6;
+    bool ssl;
 public:
-	IrcServerHostConfiguration(const std::string& hostName,
-							   int port,
-							   const std::string& password,
-							   bool ipV6,
-							   bool ssl);
+    IrcServerHostConfiguration(const std::string& hostName,
+                               int port,
+                               const std::string& password,
+                               bool ipV6,
+                               bool ssl);
 
-	std::string getHostName() const;
-	int getPort() const;
-	std::string getPassword() const;
-	bool getIpV6() const;
-	bool getSsl() const;
+    std::string getHostName() const;
+    int getPort() const;
+    std::string getPassword() const;
+    bool getIpV6() const;
+    bool getSsl() const;
 };
 
 #endif
