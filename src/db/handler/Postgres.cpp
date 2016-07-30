07e9367837f660fd5eadd610456faae34cded717
Manuel Herrmann <0@0x17.de>
2016-07-31 00:13:57 +0200

    postgres database preparations
    Signed-off-by: Manuel Herrmann <0@0x17.de>

---
+++ src/db/handler/Postgres.cpp
@@ -0,0 +1,37 @@
+#include "Postgres.hpp"
+#include "event/EventQuit.hpp"
+#include "event/EventInit.hpp"
+
+using namespace std;
+
+
+namespace Database {
+
+    struct Postgres_Impl {
+        EventQueue* appQueue;
+        Postgres_Impl(EventQueue* appQueue) : appQueue{appQueue} {};
+        bool onEvent(std::shared_ptr<IEvent> event);
+        friend Postgres;
+    };
+
+    Postgres::Postgres(EventQueue* appQueue)
+        : EventLoop{}
+        , impl{make_shared<Postgres_Impl>(appQueue)}
+    {
+    }
+
+    bool Postgres::onEvent(std::shared_ptr<IEvent> event) {
+        return impl->onEvent(event);
+    }
+
+    bool Postgres_Impl::onEvent(std::shared_ptr<IEvent> event) {
+        UUID eventType = event->getEventUuid();
+        if (eventType == EventQuit::uuid) {
+            return false;
+        } else if (eventType == EventInit::uuid) {
+
+        }
+        return true;
+    }
+
+}
