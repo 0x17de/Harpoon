71861c6cde7d7cbe7edf6337de18f03fe0a00c8a
Manuel Herrmann <0@0x17.de>
2016-08-01 02:32:53 +0200

    connect to database
    Signed-off-by: Manuel Herrmann <0@0x17.de>

--- src/db/handler/Postgres.cpp
+++ src/db/handler/Postgres.cpp
@@ -2,6 +2,10 @@
 #include "utils/ModuleProvider.hpp"
 #include "event/EventQuit.hpp"
 #include "event/EventInit.hpp"
+#include "utils/Ini.hpp"
+
+#include <iostream>
+#include <soci/soci.h>
 
 using namespace std;
 
@@ -15,6 +19,9 @@ namespace Database {
         EventQueue* appQueue;
         Postgres_Impl(EventQueue* appQueue) : appQueue{appQueue} {};
         bool onEvent(std::shared_ptr<IEvent> event);
+
+        shared_ptr<soci::session> sqlSession;
+
         friend Postgres;
     };
 
@@ -33,7 +40,29 @@ namespace Database {
         if (eventType == EventQuit::uuid) {
             return false;
         } else if (eventType == EventInit::uuid) {
+            using namespace soci;
 
+            string username, password, database;
+
+            Ini dbIni("config/postgres.ini");
+            auto& auth = dbIni.expectCategory("auth");
+            dbIni.getEntry(auth, "username", username);
+            dbIni.getEntry(auth, "password", password);
+            dbIni.getEntry(auth, "database", database);
+
+#warning check if values are 'evil'
+            stringstream login;
+            login << "postgresql://"
+                  << "dbname=" << database << " "
+                  << "user=" << username << " "
+                  << "password=" << password;
+
+            try {
+                sqlSession = make_shared<soci::session>(login.str());
+            } catch(soci_error& e) {
+                cout << "Could not connect to database server. Reason: " << endl << e.what() << endl << endl;
+                return false;
+            }
         }
         return true;
     }
