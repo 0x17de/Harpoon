4fadc7c838fbe2d370d9263919e18ec366d5d787
Manuel Herrmann <0@0x17.de>
2016-08-03 00:05:12 +0200

    irc backlog preparations
    Signed-off-by: Manuel Herrmann <0@0x17.de>

--- src/service/irc/IrcBacklogService.hpp
+++ src/service/irc/IrcBacklogService.hpp
@@ -2,10 +2,14 @@
 #define IRCBACKLOGSERVICE_H
 
 #include "queue/EventLoop.hpp"
+#include <list>
 
 
 class IrcBacklogService : public EventLoop {
     EventQueue* appQueue;
+    bool databaseInitialized;
+    std::list<std::shared_ptr<IEvent>> heldBackEvents;
+    bool processEvent(std::shared_ptr<IEvent>);
 public:
     IrcBacklogService(EventQueue* appQueue);
     virtual bool onEvent(std::shared_ptr<IEvent> event) override;
