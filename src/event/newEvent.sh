878c469434eac20f92e92edccbff7475fe5f257d
Manuel Herrmann <git@0x17.de>
2016-08-13 00:28:00 +0200

    removed old script

--- src/event/newEvent.sh
+++
@@ -1,6 +0,0 @@
-if [ "x$1" == "x" ]; then
-	echo "Generates Files: Event\${ARG1}.{cpp,hpp}"
-fi
-
-sed -r "s/TEMPLATE/\\U${1}/g; s/Template/${1}/g" EventTemplate.cpp > Event${1}.cpp
-sed -r "s/TEMPLATE/\\U${1}/g; s/Template/${1}/g" EventTemplate.hpp > Event${1}.hpp
