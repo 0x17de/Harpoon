ceb36088f8624167d31c3447fca24f87f82b9887
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2016-10-27 22:12:13 +0200

    add MessageType to EventIrcMessage

--- src/event/irc/EventIrcMessage.hpp
+++ src/event/irc/EventIrcMessage.hpp
@@ -2,6 +2,7 @@
 #define EVENTIRCMESSAGE_H
 
 #include "../IClientEvent.hpp"
+#include "EventIrcMessageType.hpp"
 #include "IrcLoggable.hpp"
 #include <string>
 
@@ -12,17 +13,24 @@ class EventIrcMessage : public IClientEvent, public IrcLoggable {
     std::string from;
     std::string channel;
     std::string message;
+    MessageType type;
 public:
     static UUID uuid;
     virtual UUID getEventUuid() const override;
 
-    EventIrcMessage(size_t userId, size_t serverId, const std::string& from, const std::string& channel, const std::string& message);
+    EventIrcMessage(size_t userId,
+                    size_t serverId,
+                    const std::string& from,
+                    const std::string& channel,
+                    const std::string& message,
+                    MessageType type);
 
     virtual size_t getUserId() const override;
     size_t getServerId() const;
     std::string getFrom() const;
     std::string getChannel() const;
     std::string getMessage() const;
+    MessageType getType() const;
 };
 
 #endif
