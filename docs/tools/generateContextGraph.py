8893173ae0f61b8770f006d2a655ec15549804f2
Manuel Herrmann <0@0x17.de>
2017-02-12 01:21:46 +0100

    inheritance graph

--- docs/tools/generateContextGraph.py
+++ docs/tools/generateContextGraph.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import re
 import pathlib
+import itertools
 
 moduleMap={}
 
@@ -33,9 +34,34 @@ def findModuleProviders():
                 break
     return res
 
+def findInheritance():
+    res = []
+    class_regexp = re.compile('^[ \t]*class .*(public|private|protected)')
+
+    step1_rewrite = re.compile('^[ \t]*class ([^ ]+)[ ]*:')
+    step2_remove_scope = re.compile('[ \t]*(public|private|protected)[ \t]+')
+    step3_remove_spaces = re.compile('[ \t]*,[ \t]*')
+    step4_fix_ending = re.compile('[ \t]*{$')
+
+    p = pathlib.Path('.')
+    for filepath in itertools.chain(p.glob('src/**/*.cpp'), p.glob('src/**/*.hpp')):
+        filepath = str(filepath)
+        with open(filepath, 'r') as f:
+            lines = f.read().split('\n')
+        for line in lines:
+            if not class_regexp.search(line) is None:
+                line = re.sub(step1_rewrite, '\"\\1\" -> { "', line, count=1)
+                line = re.sub(step2_remove_scope, '', line)
+                line = re.sub(step3_remove_spaces, '", "', line)
+                line = re.sub(step4_fix_ending, '" }', line, count=1)
+                res.append(line)
+    return res
+
+
 if __name__ == '__main__':
     modules=findUsedModules()
     providers=findModuleProviders()
+    inheritance=findInheritance()
 
     with open('docs/input/initialization.dot', 'w') as f:
         f.write('digraph Initialization {\n')
@@ -48,5 +74,9 @@ if __name__ == '__main__':
             f.write('servprov -> "'+provider['class']+'\n'+provider['file']+'" [label="'+provider['category']+':'+provider['name']+'"];\n')
         f.write('}\n')
 
-
+    with open('docs/input/inheritance.dot', 'w') as f:
+        f.write('digraph Initialization {\n')
+        f.write('rankdir=LR;')
+        f.write('\n'.join(inheritance))
+        f.write('\n}\n')
 
