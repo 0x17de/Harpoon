20688f2d3851bea4ddbb792adbab5ea34eb1eb8b
Manuel Herrmann <0@0x17.de>
2017-04-13 01:47:28 +0200

    eventloop docs

--- src/queue/EventLoop.hpp
+++ src/queue/EventLoop.hpp
@@ -9,6 +9,7 @@
 #include "queue/EventQueue.hpp"
 
 
+/// Used to filter for only specific events. A dynamic_cast is used to filter by event class.
 template<class T>
 bool EventGuard(IEvent* event) {
     return dynamic_cast<T*>(event) != nullptr;
@@ -22,12 +23,18 @@ class EventLoop {
     std::thread t;
     bool destruction;
 protected:
+    /// Callback for when events are received
     virtual bool onEvent(std::shared_ptr<IEvent> event) = 0;
 public:
+    /// Default constructor
     EventLoop();
+    /// Eventloop which can be run in the current thread or in the background
     explicit EventLoop(const std::set<UUID>& processableEvents, const std::list<bool(*)(IEvent*)>& = {}, bool threaded = true);
+    /// Destructor
     virtual ~EventLoop();
+    /// Automatically called in the threaded mode. Otherwise call this to block until the event loop is finished.
     void run();
+    /// Accessor for the EventQueue instance.
     EventQueue* getEventQueue();
 };
 
