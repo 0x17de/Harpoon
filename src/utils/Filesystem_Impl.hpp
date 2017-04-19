ed932ddd4605cdc1f92ae3af9f4c3e04c661f9ca
Manuel Herrmann <0@0x17.de>
2017-04-19 20:10:37 +0200

    docs

--- src/utils/Filesystem_Impl.hpp
+++ src/utils/Filesystem_Impl.hpp
@@ -11,8 +11,11 @@ public:
 	std::recursive_mutex lock;
 	std::string root;
 
+    /// Returns the current working directory
 	std::string currentDirectory();
+    /// Creates a directory with specified access permissions
 	bool createDirectory(const std::string& path, mode_t mode);
+    /// Changes into a directory
 	bool changeDirectory(const std::string& path);
 };
 
--- src/utils/ModuleProvider.hpp
+++ src/utils/ModuleProvider.hpp
@@ -7,10 +7,13 @@
 #include <string>
 #include <functional>
 
+/// Registers a basic module for the specified 'target' - e.g. EventLoop
 #define PROVIDE_MODULE(target, category, name, className)               \
     namespace { ProvideModule<target, className> provider(category, name); }
+/// PROVIDE_MODULE: Simplified for EventLoop
 #define PROVIDE_EVENTLOOP_MODULE(category, name, className)             \
     namespace { ProvideModule<EventLoop, className> provider(category, name); }
+/// PROVIDE_MODULE: Simplified for WebsocketProtocolHandler
 #define PROVIDE_WEBSOCKETPROTOCOL_MODULE(category, name, className)             \
     namespace { ProvideModule<WebsocketProtocolHandler, className> provider(category, name); }
 
@@ -20,9 +23,12 @@ class EventQueue;
 class EventLoop;
 
 
+/// Template for all module types
+/// The init function can be different for each module
 template<class Target>
 struct ModuleInitializer { };
 
+/// Init template specialized for the EventLoop type
 template<>
 struct ModuleInitializer<EventLoop> {
     using return_type = std::shared_ptr<EventLoop>;
@@ -34,6 +40,7 @@ struct ModuleInitializer<EventLoop> {
     }
 };
 
+/// Init template specialized for the WebsocketProtocolHandler type
 template<>
 struct ModuleInitializer<WebsocketProtocolHandler> {
     using return_type = std::shared_ptr<WebsocketProtocolHandler>;
@@ -46,7 +53,10 @@ struct ModuleInitializer<WebsocketProtocolHandler> {
 };
 
 
-
+/// Modules register at the module provider.
+/// Others can use the module provider to initialize instances
+/// of the corresponding type. See ModuleInitializer.
+/// Macros are used to register at the ModuleProvider.
 template<class Target>
 class ModuleProvider {
     using callback_type = typename ModuleInitializer<Target>::callback_type;
@@ -55,11 +65,13 @@ class ModuleProvider {
 
     ModuleProvider() = default;
 public:
+    /// Get the static instance of a module provider
     static ModuleProvider& getInstance() {
         static ModuleProvider moduleProvider;
         return moduleProvider;
     }
 
+    /// Returns the function for initialization of some module
     typename ModuleInitializer<Target>::callback_type getInitializerFunction(const std::string& category,
                                         const std::string& name) const {
         auto categoryIt = initializerByCategory.find(category);
@@ -78,6 +90,11 @@ public:
         return initializerIt->second;
     }
 
+    /// Registers a module with its callback
+    ///
+    /// \param category Multiple modules can provide the same functionality. Example: Database
+    /// \param name The module's name
+    /// \param initializer Callback to initialize some instance of the module
     void registerModule(const std::string& category,
                         const std::string& name,
                         const typename ModuleInitializer<Target>::callback_type& initializer) {
@@ -85,6 +102,7 @@ public:
         initializerMap.emplace(name, initializer);
     }
 
+    /// Helper function to iterate through all existing modules
     template<class Fn>
     void forEachModule(const std::string& category, Fn f) {
         auto it = initializerByCategory.find(category);
@@ -94,6 +112,7 @@ public:
     }
 };
 
+/// Helper template class to globally register modules on application start
 template<class Target, class ModuleClass>
 class ProvideModule {
 public:
