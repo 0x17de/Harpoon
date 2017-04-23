1e07a2a8e17ffb06bf35db1da320867af2a0ceb9
Manuel Herrmann <0@0x17.de>
2017-04-23 17:27:16 +0000

    docs

--- src/app/ApplicationGuard.hpp
+++ src/app/ApplicationGuard.hpp
@@ -5,11 +5,15 @@
 
 
 class Application;
+/// Sets the signal handler and sets the application instance variable used by the handler
 class ApplicationGuard {
 public:
+    /// List of applications using the signal handler
     static std::list<Application*> apps;
     Application* app;
+    /// Sets the handler and inserts the application into the list
     explicit ApplicationGuard(Application* app);
+    /// Removes the application from the list but keeps the signal handlers
     ~ApplicationGuard();
 };
 
