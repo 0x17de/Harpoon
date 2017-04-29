85eac01cda6d2effc0a7db0ac35412c4ae0a9c2e
Manuel Herrmann <0@0x17.de>
2017-04-29 20:57:55 +0200

    docs

--- src/event/EventDatabaseQuery.hpp
+++ src/event/EventDatabaseQuery.hpp
@@ -9,16 +9,19 @@
 
 namespace Query { struct QueryBase; }
 class EventQueue;
+/// Event for abstract database queries to a database module
 class EventDatabaseQuery : public IDatabaseEvent {
     EventQueue* target;
     std::list<std::unique_ptr<Query::QueryBase>> queries;
     std::shared_ptr<IEvent> eventOrigin;
 
+    /// termination condition
     template<int n, class... T>
-    typename std::enable_if<n == sizeof...(T)>::type addQueries(std::tuple<T...>& query) {
+    inline typename std::enable_if<n == sizeof...(T)>::type addQueries(std::tuple<T...>& query) {
     }
+    /// recursively adds queries
     template<int n, class... T>
-    typename std::enable_if<n != sizeof...(T)>::type addQueries(std::tuple<T...>& query) {
+    inline typename std::enable_if<n != sizeof...(T)>::type addQueries(std::tuple<T...>& query) {
         queries.push_back(std::move(std::get<n>(query)));
         addQueries<n+1>(query);
     }
@@ -27,6 +30,7 @@ public:
     static constexpr UUID uuid = 1;
     virtual UUID getEventUuid() const override;
 
+    /// Constructor adding all supplied queries into the event
     template<class... T>
     EventDatabaseQuery(EventQueue* target, const std::shared_ptr<IEvent>& eventOrigin, T&&... t)
         : target{target}
@@ -36,8 +40,11 @@ public:
         addQueries<0>(tuple);
     }
 
+    /// Returns the requesting event queue. The only target that will be notified by the results
     EventQueue* getTarget() const;
+    /// Returns the original event that was initiator for the database request. E.g. A chat message event.
     std::shared_ptr<IEvent> getEventOrigin() const;
+    /// Returns the const reference to all queries
     const std::list<std::unique_ptr<Query::QueryBase>>& getQueries() const;
 };
 
