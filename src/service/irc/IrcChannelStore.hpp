7dfe9f3ec8de9d56b02918de7d6cc2fe3c9f8afb
Manuel Herrmann <0@0x17.de>
2017-05-07 21:36:08 +0200

    docs

--- src/service/irc/IrcChannelStore.hpp
+++ src/service/irc/IrcChannelStore.hpp
@@ -31,16 +31,34 @@ public:
     IrcChannelStore(const std::string& channelPassword,
                     bool disabled);
 
+    /// remove all users from the channel
     void clear();
+    /// add a single user to the channel
     void addUser(const std::string& nick, const std::string& mode);
+    /// remove a single user from the channel
     void removeUser(const std::string& nick);
+    /// rename a user
+    ///
+    /// \param nick Old nickname
+    /// \param newNick New nickname
     void renameUser(const std::string& nick, const std::string& newNick);
+    /// Change the irc user mode of a user
+    ///
+    /// \param nick Current nickname
+    /// \param mode New mode char
+    /// \param add True to add or false to remove a mode
     void changeUserMode(const std::string& nick, char mode, bool add);
+    /// Returns the map of users to stored data
     const std::map<std::string, IrcUserStore>& getUsers() const;
+    /// Returns the channel's password that is currently used
     std::string getChannelPassword() const;
+    /// True if the channel is disabled
     bool getDisabled() const;
+    /// Disable or enable a channel
     void setDisabled(bool ldisabled);
+    /// Returns the topic string
     std::string getTopic() const;
+    /// Sets the topic string
     void setTopic(const std::string& topic);
 };
 
